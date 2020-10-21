Correct accuracy computation in `OracleCollaborativeAccuracy`.
Compute model accuracy using real accuracy (`num_correct / counts`) from each bin rather than the max value of each bin. The previous method can make the collaborative accuracy lower than the original accuracy when the model is underconfident (e.g., when combined with focal loss).

PiperOrigin-RevId: 338173322

uncertainty_metrics/tensorflow/accuracy.py
@@ -91,7 +91,7 @@ class OracleCollaborativeAccuracy(calibration.ExpectedCalibrationError):
       elif num_oracle_examples_so_far < num_oracle_examples:
         # Send num_correct_oracle examples in the current bin to oracle,
         # and have model to predict the rest.
-        cur_bin_accuracy = _bin_probabilities(self.num_bins, i, self.dtype)
+        cur_bin_accuracy = cur_bin_num_correct / cur_bin_counts
         num_correct_oracle = tf.cast(
             num_oracle_examples - num_oracle_examples_so_far, self.dtype)
         num_correct_model = (cur_bin_counts -

uncertainty_metrics/tensorflow/accuracy_test.py
@@ -36,8 +36,11 @@ class OracleCollaborativeAccuracyTest(parameterized.TestCase, tf.test.TestCase):
     bin_correct_sums = np.array([0, 0, 0, 0, 0, 1, 2, 0, 2, 0])
     bin_prob_sums = np.array(
         [0, 0, 0, 0, 0, 0.51 + 0.55, 0.61 + 0.66 + 0.68, 0.71, 0.81 + 0.85, 0])
+    # `(3 - 1)` refers to the rest examples in this bin
+    # (minus the examples sent to the moderators), while `2/3` is
+    # the accuracy in this bin.
     bin_collab_correct_sums = np.array(
-        [0, 0, 0, 0, 0, 2, 1 * 1.0 + (3 - 1) * 0.7, 0, 2, 0])
+        [0, 0, 0, 0, 0, 2, 1 * 1.0 + (3 - 1) * (2 / 3), 0, 2, 0])
 
     correct_acc = np.sum(bin_collab_correct_sums) / np.sum(bin_counts)
 